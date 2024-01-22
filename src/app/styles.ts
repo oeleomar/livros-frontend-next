d95632565721347268da0618250d3e6333f9ca34
oeleomar <eleomar.dev@gmail.com>
2024-01-21 22:37:57 -0400

    Update language and styling in layout component

--- src/app/styles.ts
+++ src/app/styles.ts
@@ -1,9 +1,17 @@
 import styled, { css } from "styled-components";
 
 export const Wrapper = styled.div`
-  ${({ theme }) => css``}
+  ${({ theme }) => css`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  `}
 `;
 
-export const Container = styled.div`
-  ${({ theme }) => css``}
+export const Container = styled.main`
+  ${({ theme }) => css`
+    width: 80%;
+    max-width: 80rem;
+    background-color: red;
+  `}
 `;
